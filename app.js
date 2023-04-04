72b8b18ed684e006ad1bfc25aca7d36574393568
ridertus <ridertus@hotmail.com>
2023-04-05 04:46:14 +0700

    Connect sign up, sign in, and database
     to create finish sign up and sign in system

--- app.js
+++ app.js
@@ -4,16 +4,55 @@ const bodyParser = require('body-parser');
 const sass = require('node-sass');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
 const db = require("./config/db.js");
-const user = require('./model/user');
+const User = require('./model/user');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-  
+
+app.use(session({
+    secret: 'secret key',
+    resave: false,
+    saveUninitialized: false
+}));
+
 db.connect();
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+	User.findOne({ email: email })
+		.then((user) => {
+			if (!user) {
+				return done(null, false, { message: 'Incorrect email.' });
+			}
+			if (user.password !== password) {
+				return done(null, false, { message: 'Incorrect password.' });
+			}
+			return done(null, user);
+		})
+		.catch((err) => done(err));
+}));
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then((user) => {
+            done(null, user);
+        })
+        .catch((err) => {
+            done(err, null);
+        });
+});
 
 //Sign Up
 app.get('/sign_up', (req, res) => {
@@ -21,7 +60,7 @@ app.get('/sign_up', (req, res) => {
 });
 app.post('/sign_up', (req, res) => {
 	const { username, email, password } = req.body;
-	const newUser = new user({
+	const newUser = new User({
 		username,
 		email,
 		password
@@ -40,7 +79,11 @@ app.post('/sign_up', (req, res) => {
 app.get('/sign_in', (req, res) => {
     res.render('sign_in');
 });
-
+app.post('/sign_in', passport.authenticate('local', {
+	successRedirect: '/home',
+	failureRedirect: '/sign_in',
+	failureFlash: true
+}));
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
