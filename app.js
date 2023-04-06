75ae2b718c9ae8a64f20b27744850af4aac8e61a
ridertus <ridertus@hotmail.com>
2023-04-06 10:23:23 +0700

    add soft-drink page

--- app.js
+++ app.js
@@ -243,6 +243,27 @@ app.get('/beverage', async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+// Soft-drink
+app.get('/soft-drink', async (req, res) => {
+  try {
+    if (req.user && req.user.email) {
+      const user = await User.findOne({ email: req.user.email });
+      const username = user.username;
+
+      const products = await Product.find();
+      const orders = req.user.order;
+
+      res.render("soft-drink", { username: username, user: user, products: products, orders: orders });
+      
+    } else {
+      res.redirect("/sign_in");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal server error");
+  }
+});
+
 
 
 app.listen(3000, function() {
